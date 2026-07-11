73d3755d36fde81f4f7a49185e660bae262aa8f2
Huy Nguyen <ndhgl99@gmail.com>
2026-07-11 23:28:29 +0700

    Disable edit-page button on posts

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -23,8 +23,7 @@ export default defineAstroPaperConfig({
     showArchives: true,
     showBackButton: true,
     editPost: {
-      enabled: true,
-      url: "https://github.com/huynguyengl99/huynguyengl99.github.io/edit/main/",
+      enabled: false,
     },
     search: "pagefind",
   },
